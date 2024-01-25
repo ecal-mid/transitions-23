39b8ff923cbaf0890405b619848c318e1e7a9c59
Mario von Rickenbach <mario@mariov.ch>
2024-01-25 11:22:39 +0100

    try fix sound on mobile

--- shared.js
+++ shared.js
@@ -3,7 +3,9 @@
 
 document.addEventListener('touchstart', async () => {
 
-    await getAudioContext().resume() // fix audio on iOS
+    var a = getAudioContext()
+    if( a.state === "suspended")
+        await a.resume() // fix audio on iOS
 
 }, { capture: true }); // add a dummy listener to fix touch in iframes on iOS
 
